d40c859c8f29f5d25333e4029aad2b4969e20cae
YasirKhan231 <yk4987746@gmail.com>
2025-07-20 12:05:51 +0530

    ui change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/home";
+import Home from "./pages/Home/Home";
 import Access from "./pages/Access/Access";
 import { CourseProvider } from "./context/coursecontext";
 import styles from "./App.module.css";
